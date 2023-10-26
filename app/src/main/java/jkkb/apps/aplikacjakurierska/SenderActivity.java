4a51d99f8a3e6d97b37bdde095fd90f46db3cac4
Jacek <jacek.kurzydlo@protonmail.ch>
2023-10-26 11:55:10 +0200

    Dodano mechanizm skanowania kodów QR

---
+++ app/src/main/java/jkkb/apps/aplikacjakurierska/SenderActivity.java
@@ -0,0 +1,14 @@
+package jkkb.apps.aplikacjakurierska;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sender);
+    }
+}
